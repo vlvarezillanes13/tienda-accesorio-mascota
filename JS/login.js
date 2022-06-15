e48f1d57d502c7050fc6f10c109afaf84239406f
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2022-06-15 18:16:32 -0400

    validación login

---
+++ JS/login.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    $("#form-login").validate({
+        rules:{
+            correo:{
+                required:true,
+                email:true
+            },
+            contraseña:{
+                required:true,
+                minlength:3
+            }
+        },
+        messages:{
+            correo:{
+                required:"Debe ingresar un correo!",
+                email:"Debe ingresar un correo valido!"
+            },
+            contraseña:{
+                required:"Debe ingresar una contraseña!",
+                minlength:"Contraseña invalida!"
+            }
+        }
+
+    });
+
+});
