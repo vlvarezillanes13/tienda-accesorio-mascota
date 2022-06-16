0f4d6f022ffb487d6378d8568a70f5c59e6ee605
G4rrow <gp.roaq@gmail.com>
2022-06-15 23:17:44 -0400

    Validacion de contacto

---
+++ JS/contacto.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+    $("#form-contacto").validate({
+        rules:{
+            nombre:{
+                required:true,
+                minlength: 2
+            },
+            apellido:{
+                required:true,
+                minlength: 2
+            },
+            emailinput:{
+                required:true,
+                email:true
+            },
+            asunto:{
+                required:true,
+                
+            },
+            descripcion:{
+                required:true,
+                
+            }
+        },
+        messages:{
+            nombre:{
+                required:"Debe ingresar su apellido",
+                minlength:"Debe tener como minimo 2 caracteres"
+            },
+            apellido:{
+                required:"Debe ingresar su apellido",
+                minlength:"Debe tener como minimo 2 caracteres"
+            },
+            emailinput:{
+                required:"Ingrese su correo",
+                email:"Debe ingresar un correo valido"
+            },
+            asunto:{
+                required:"Por favor ingrese un asunto",
+                
+            },
+            descripcion:{
+                required:"Por favor ingresa un comentario",
+                
+            },            
+        }
+
+    });
+
+});
